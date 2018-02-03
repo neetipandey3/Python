5db3d541f2adf8458273933bd5e2e6888568315c
neetipandey3 <31138437+neetipandey3@users.noreply.github.com>
2018-02-02 16:09:28 -0800

    Add files via upload
    implementation of unbounded Knapsack problem

---
+++ KnapsackHR.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+#import numpy
+
+from sys import setrecursionlimit
+limit = 1000000
+setrecursionlimit(limit)
+
+
+def unboundedKnapsack(k, arr):
+    # Complete this function
+    maxInstances = 2000
+    maxWeight = 2000
+    knapsack = [[0] * maxInstances for _ in range(maxWeight)]
+    #knapsack = numpy.zeros((maxInstances, maxWeight), dtype = int)
+
+    for i in range(1, len(arr)+1):
+        for j in range(1, k+1):
+            if j >= arr[i-1]:
+                knapsack[i][j] = max(knapsack[i-1][j], knapsack[i][j-arr[i-1]] + arr[i-1])
+            else:
+                knapsack[i][j] = knapsack[i-1][j]
+    return knapsack[len(arr)][k]
+
+
+
+if __name__ == "__main__":
+    result = []
+    t = int(input().strip())
+
+    for _ in range(0, t):
+        n, k = input().strip().split(' ')
+        n, k = [int(n), int(k)]
+        arr = list(map(int, input().strip().split(' ')))
+        result.append(unboundedKnapsack(k, arr))
+    for each in result:
+        print(each)
+
